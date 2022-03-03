3615fecb133444980b3c9aad242582682fbfccde
rayner-villalba-coderoad-com <rayner.villalba@coderoad.com>
2022-03-03 13:39:58 -0400

    Agregando mi segundo cambio

--- caso_de_estudio/simulador_cajero_automatico/src/PruebaCajeroAutomatico.java
+++ caso_de_estudio/simulador_cajero_automatico/src/PruebaCajeroAutomatico.java
@@ -1,6 +1,21 @@
+//Paso 1 importamos scanner
+import java.util.Scanner;
+
 public class PruebaCajeroAutomatico{
     public static void main(String[ ] args )
     {
+        //Paso 2 instanciar para realizar la lectura de datos
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("*******************************************");
         System.out.println("Cajero Automático");
+        System.out.println("*******************************************");
+        System.out.println("Bienvenido!");
+        System.out.println();
+        System.out.print("Escriba su PIN!");
+
+        //Leer el pin desde teclado
+        int pin = scanner.nextInt();
+
     }
 }
