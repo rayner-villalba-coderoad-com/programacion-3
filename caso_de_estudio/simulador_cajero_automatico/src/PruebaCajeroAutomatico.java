f6d4ceeae0716a4b25c47efa35fedb22c73f72dc
rayner-villalba-coderoad-com <rayner.villalba@coderoad.com>
2022-03-03 14:10:04 -0400

    Agregando menu

--- caso_de_estudio/simulador_cajero_automatico/src/PruebaCajeroAutomatico.java
+++ caso_de_estudio/simulador_cajero_automatico/src/PruebaCajeroAutomatico.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class PruebaCajeroAutomatico{
     public static void main(String[ ] args )
     {
+        int defaultPin = 1234;
         //Paso 2 instanciar para realizar la lectura de datos
         Scanner scanner = new Scanner(System.in);
 
@@ -16,6 +17,19 @@ public class PruebaCajeroAutomatico{
 
         //Leer el pin desde teclado
         int pin = scanner.nextInt();
+
+        if (defaultPin == pin) {
+            System.out.println("Menu PRINCIPAL");
+            System.out.println("=================================");
+            System.out.println("1 - Ver mi Saldo");
+            System.out.println("2 - Retirar mi efectivo");
+            System.out.println("3 - Depositar fondos");
+            System.out.println("4 - Salir");
+            System.out.print("Ingrese su opcion: ");
+            int opcion = scanner.nextInt();
+        } else {
+            System.out.print(" PIN incorrecto");
+        }
 
     }
 }
