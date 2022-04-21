4ab1f9fe5e3bea011c5594c17144b9639c967ec2
rayner-villalba-coderoad-com <rayner.villalba@mojix.com>
2022-04-21 13:57:56 -0400

    Aprendiendo de contenedores con Jpanel

--- ejercicios/BorderLayoutVentana/src/com/unifranz/BorderLayoutVentanita.java
+++ ejercicios/BorderLayoutVentana/src/com/unifranz/BorderLayoutVentanita.java
@@ -10,12 +10,21 @@ public class BorderLayoutVentanita extends JFrame {
     JButton button3;
     JButton button4;
     JButton button5;
+    JPanel panel1;
+    JPanel panel2;
+
 
     //Contructor
     public BorderLayoutVentanita() {
         super("BorderLayout");
         //Definiendo borderlayout
         setLayout(new BorderLayout());
+        //contenedor
+        panel1 = new JPanel();
+        panel2 = new JPanel();
+
+        panel1.setLayout(new BorderLayout(3, 4));
+        panel2.setLayout(new GridLayout(2, 2));
 
         //Definiendo mis botones
         button1 = new JButton("Sur");
@@ -24,12 +33,22 @@ public class BorderLayoutVentanita extends JFrame {
         button4 = new JButton("Este");
         button5 = new JButton("Centro");
 
-        add(button1, "South");
-        add(button2, "West");
-        add(button3, "North");
-        add(button4, "East");
-        add(button5, "Center");
+        panel1.add(button1, "South");
+        //add(button1, "South");
+        panel1.add(button2, "West");
+        panel1.add(button3, "North");
+        panel1.add(button4, "East");
+        panel1.add(button5, "Center");
 
+        this.getContentPane().add(panel1, "South");
+
+
+        panel2.add(new JButton("1"));
+        //panel2.add(new JButton("2"));
+        panel2.add(new JButton("3"));
+        panel2.add(new JButton("4"));
+
+        this.getContentPane().add(panel2, "North");
         //Dimensionar la ventana de acuerdo al ancho y alto
         setSize(400, 400);
 
