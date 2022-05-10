23787fc2ab54c10f6f16f0666880ee15e4340588
rayner-villalba-coderoad-com <rayner.villalba@mojix.com>
2022-05-10 14:15:50 -0400

    Agregando el sistema de VideoClub

---
+++ caso_de_estudio/VideoclubUnifranz/src/videoclubunifranz/VideoclubUnifranz.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package videoclubunifranz;
+
+/**
+ *
+ * @author rayner
+ */
+public class VideoclubUnifranz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("SISTEMA VIDEO CLUB");
+        Login pantallaDeInicioDeSesion = new Login();
+        
+        //Mostrar al centro de la pantalla
+        pantallaDeInicioDeSesion.setLocationRelativeTo(null);
+        pantallaDeInicioDeSesion.setVisible(true);
+        
+       
+    }
+    
+}
